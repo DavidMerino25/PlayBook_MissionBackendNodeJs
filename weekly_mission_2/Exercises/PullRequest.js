d8de15f2de5af0554f18605aac84879e2c0ba1e4
David2508 <deividrocan2508@gmail.com>
2022-04-16 23:49:40 -0500

    add: Pull

--- weekly_mission_2/Exercises/PullRequest.js
+++ weekly_mission_2/Exercises/PullRequest.js
@@ -1,3 +1,14 @@
 const PullRequest ={
-    
+    title:"Create my Code",
+    branchname:"main",
+    dateCreated: Date(),
+    status:"Acepted",
+    repositoryNameAssociated:"Launh-PlayBook",
+
+    getStatus: function(){
+        return this.status
+    },
+    getTitleandAutor: function(){
+        
+    }
 }
