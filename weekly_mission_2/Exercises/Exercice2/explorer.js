527b4a25bddbba9905481bb76285d9bc9aaafb03
David2508 <deividrocan2508@gmail.com>
2022-04-18 11:26:30 -0500

    add: stack

---
+++ weekly_mission_2/Exercises/Exercice2/explorer.js
@@ -0,0 +1,65 @@
+const explorers = [
+    {
+      name: "Explorer 1",
+      exercises_completed: 10,
+      rate: 99,
+      city: "CDMX",
+      stack: [
+        "js",
+        "c#"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: true,
+          exercisesFinished: true
+        }
+      }
+    },
+    {
+      name: "Explorer 2",
+      exercises_completed: 9,
+      city: "Veracruz",
+      rate: 50,
+      stack: [
+        "js"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: false,
+          exercisesFinished: false
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+    {
+      name: "Explorer 3",
+      exercises_completed: 3,
+      city: "Sonora",
+      rate: 100,
+      stack: [
+        "elixir"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    }
+   ]
+
+console.log("Ejercicio 1 Imprimiendo nombres con foreach");
+explorers.forEach(explorer => console.log(explorer.name))
+console.log("Ejercicio  Imprimiendo stacks con foreach");
+explorers.forEach(explorer => console.log(`Stack de ${explorer.name}: ${explorer.stack}`))
